1e400b3ef8ab6e24175f48f2eda759e05a5783a9
longdragon11 <157029204+longdragon11@users.noreply.github.com>
2025-05-23 11:17:57 +0800

    Update geoML29.MR.R

--- geoML29.MR.R
+++ geoML29.MR.R
@@ -167,12 +167,3 @@ for(outcomeFile in files){
 	write.csv(outTab, file=paste0(outcomeID, "__all.MRresult.csv"), row.names=F)
 }
 
-
-######生信自学网: https://www.biowolf.cn/
-######课程链接1: https://shop119322454.taobao.com
-######课程链接2: https://ke.biowolf.cn
-######课程链接3: https://ke.biowolf.cn/mobile
-######光俊老师邮箱: seqbio@foxmail.com
-######光俊老师微信: eduBio
-
-
